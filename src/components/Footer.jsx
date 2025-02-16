80f7cb5b3e7af92b0f2e1dd37b5c2709693d4379
jhonnydrummer <arantes.designergrafico@gmail.com>
2025-02-16 20:45:23 +0000

    ddd

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,12 +9,16 @@ const Footer = () => {
       {/* Footer */}
       <footer className="footer-footer">
         <div className="links-termos-sobre">
-          <Link to="/termos-de-uso-e-privacidade" className="nav-items">
-            Termos de Uso e Privacidade
-          </Link>
-          <Link to="/sobre-nos" className="nav-items">
-            Sobre Nós
-          </Link>
+          <div>
+            <Link to="/termos-de-uso-e-privacidade" className="nav-items">
+              Termos de Uso e Privacidade
+            </Link>
+          </div>
+          <div>
+            <Link to="/sobre-nos" className="nav-items">
+              Sobre Nós
+            </Link>
+          </div>
         </div>
         <div className="copyright">
           <p>&copy; 2025 PequiTech. Todos os direitos reservados - Site feito por <strong>PublishUp!</strong>.</p>
@@ -22,10 +26,10 @@ const Footer = () => {
         <div>
           <p>Siga-nos nas redes Sociais:</p>
           <a href="https://www.instagram.com/PequiTech" target="_blank" rel="noopener noreferrer" className="social-link">
-            <FaInstagram size={30} color="#fff" /> 
+            <FaInstagram size={30} color="#fff" />
           </a>
           <a href="https://www.youtube.com/@PequiTech" target="_blank" rel="noopener noreferrer" className="social-link">
-            <FaYoutubeSquare size={30} color="#fff" /> 
+            <FaYoutubeSquare size={30} color="#fff" />
           </a>
         </div>
       </footer>
